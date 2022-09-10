bbdffe8e7a8a7cd19f6cf3e6428df6e32fc2d241
pavaneswar kothalanka <itonystark58@gmail.com>
2022-09-10 15:41:36 +0530

    version-23.3

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,26 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import Logo from '../images/logo.jpeg'
+import { NavLink } from 'react-router-dom';
+import cart from '../images/shopping-cart.png'
+
 function Nav() {
   return (
     <div style={{borderBottom:"3px solid gray",position:"sticky",top:"0",width:"100%",backgroundColor:"white",zIndex:"2",height:"auto"}}>
         <nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-between">
-            <div  >
-            <img src={Logo} className="img-fluid" width={"50"}  />
+            <div>
+              <NavLink
+              to="/MainPage"
+              > 
+                <img src={Logo} className="img-fluid" width={"50"}  />
+              </NavLink>
             </div>
             <div>
             <h4 class="" href="#">My Homedel</h4>
             </div>
             <div>
             <ul class=" ml-md-auto mt-2" style={{courser:'pointer'}}>
-              <img src='https://cdn-icons-png.flaticon.com/512/833/833314.png' className='img-fluid' width={"30"} />
+              <img src={cart} className='img-fluid' width={"30"} />
             </ul>
             </div>
         </nav>
--- src/components/PresentOrdres.js
+++ src/components/PresentOrdres.js
@@ -1,16 +1,97 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Nav from './Nav';
 import Comp_for_home from './Comp_for_home';
- 
+import { Ip } from './../constants/Ip';
+
 function PresentOrdres() {
-  return (
-    <div> 
-    <Nav/>
+    const userid =localStorage.getItem('user');
+  
+    const [Myorders,setMyordres] = useState([]);
+   const GetItems=()=>{
+      fetch(Ip+'/GetUserOrders?id='+userid,{
+        headers:new Headers({
+          Authorization:"Bearer " 
+        })
+        }).then(res=>res.json())
+        .then(data=>{ 
+        
+         
+          setMyordres(data);
+          
+           
+           
+        }
+        )
+    }
+  
+    useEffect(()=>{
+      GetItems();
+    })
+  
+    return (
+      <div> 
+      <Nav/>
+      <div class="m_ pt-md-4 pt-3" style={{background:"rgb(255,255,255)"}}>
+                         
     
-<h1>Present Orders under construction...</h1>
-<Comp_for_home/>
-</div>
-  )
+                         {Myorders.length>0?
+     
+                         <>
+                           {Myorders.map((item)=>(
+     
+                             <div>
+                                 {item.OrderStatus!=="Delivered"?
+  
+                                 <div className='row align-items-center justify-content-center pb-2' style={{borderBottom:"1px solid lightgray"}}>
+                                     <div className='col-3'>
+                                         <img className='img-fluid rounded' src='https://b.zmtcdn.com/data/dish_photos/8d1/6df584834e5252fa5663c4e4d86618d1.jpg?fit=around|130:130&crop=130:130;*,*' />
+                                     </div>
+                                     <div className='col-3'>
+                                         <p className='font-weight-bold'>{item.OrderStatus}</p>
+                                     </div>
+                                     <div className='col-2'>
+                                         <p className='text-danger'>₹{item.Amount}</p>
+                                     </div>
+                                     <div className='col-4'>
+                                          {item.OrderStatus==="Delivered"?<h4>Done</h4>
+                                            :null
+                                          }
+                                          {item.OrderStatus==="Pending"?
+                                            <p>Waiting </p>:null
+    
+                                          }
+                                          {item.OrderStatus==="Accepted"?
+    
+                                              <p>Order Accepted</p>:null
+    
+                                          }
+                                          {item.OrderStatus==="AcceptedByDeliveryBoy"?
+    
+                                          <div className='row'>
+                                             <button className='col-10 offset-1 btn btn-secondary'>Track</button>
+                                             <button className='col-10 offset-1 btn btn-danger mt-1'>Cancel</button>
+                                         </div>:null
+    
+                                          }
+                                          {item.OrderStatus==="Declain"?
+                                            <p>Order Canceled by Restorent</p>
+                                            :null
+                                          }
+                                     </div>
+                                 </div>: null
+  
+                                 }
+                            </div>
+                           ))
+     
+                           }
+                         </>:<h1>No Orders</h1>
+                         }
+     
+                     </div>
+  <Comp_for_home/>
+      </div>
+    )
 }
 
 export default PresentOrdres;
